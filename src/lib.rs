49dd1a589450c8949568971ac81eece261d36451
John Scott <johnrscott0@gmail.com>
2023-01-06 21:54:33 +0000

    Added some initial documentation for the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,21 @@
+//! Rust interface to SuperLU, a C library for solving sparse linear systems.
+//! The crate is designed to provide a wrapper around SuperLU that is safe,
+//! performative, and retains the main features of the original SuperLU iterface.
+//!
+//! The SuperLU User Guide is
+//! <https://portal.nersc.gov/project/sparse/superlu/superlu_ug.pdf>
+//!
+//! The function reference for SuperLU is provided  
+//! <https://portal.nersc.gov/project/sparse/superlu/superlu_code_html/index.html>
+
+#![warn(missing_docs)]
 mod superlu;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+
 
 #[cfg(test)]
 mod tests {

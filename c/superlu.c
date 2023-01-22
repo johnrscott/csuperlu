a04a355b22f29fbebedf0a19baab55a8ce930259
John Scott <johnrscott0@gmail.com>
2023-01-22 12:25:39 +0000

    Did an experiment in the C function -- appears that you need to use the right precision routine (prefix d, s, etc.), but you also need to pass the right precision enum value to it, otherwise segfaults

--- c/superlu.c
+++ c/superlu.c
@@ -1,4 +1,5 @@
-#include <superlu/slu_ddefs.h>
+//#include <superlu/slu_ddefs.h>
+#include <superlu/slu_sdefs.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,8 +13,8 @@ int main(int argc, char *argv[])
      *
      */
     SuperMatrix A, L, U, B;
-    double *a, *rhs;
-    double s, u, p, e, r, l;
+    float *a, *rhs;
+    float s, u, p, e, r, l;
     int *asub, *xa;
     int *perm_r; /* row permutations from partial pivoting */
 
@@ -25,7 +26,7 @@ int main(int argc, char *argv[])
     /* Initialize matrix A. */
     m = n = 5;
     nnz = 12;
-    if ( !(a = doubleMalloc(nnz)) ) ABORT("Malloc fails for a[].");
+    if ( !(a = floatMalloc(nnz)) ) ABORT("Malloc fails for a[].");
     if ( !(asub = intMalloc(nnz)) ) ABORT("Malloc fails for asub[].");
     if ( !(xa = intMalloc(n+1)) ) ABORT("Malloc fails for xa[].");
     s = 19.0; u = 21.0; p = 16.0; e = 5.0; r = 18.0; l = 12.0;
@@ -37,14 +38,14 @@ int main(int argc, char *argv[])
     xa[0] = 0; xa[1] = 3; xa[2] = 6; xa[3] = 8; xa[4] = 10; xa[5] = 12;
 
     /* Create matrix A in the format expected by SuperLU. */
-    dCreate_CompCol_Matrix(&A, m, n, nnz, a, asub, xa, SLU_NC, SLU_D, SLU_GE);
+    sCreate_CompCol_Matrix(&A, m, n, nnz, a, asub, xa, SLU_NC, SLU_S, SLU_GE);
 
     /* Create right-hand side matrix B. */
     nrhs = 1;
 
-    if ( !(rhs = doubleMalloc(m * nrhs)) ) ABORT("Malloc fails for rhs[].");
+    if ( !(rhs = floatMalloc(m * nrhs)) ) ABORT("Malloc fails for rhs[].");
     for (i = 0; i < m; ++i) rhs[i] = 1.0;
-    dCreate_Dense_Matrix(&B, m, nrhs, rhs, m, SLU_DN, SLU_D, SLU_GE);
+    sCreate_Dense_Matrix(&B, m, nrhs, rhs, m, SLU_DN, SLU_S, SLU_GE);
 
     if ( !(perm_r = intMalloc(m)) ) ABORT("Malloc fails for perm_r[].");
     if ( !(perm_c = intMalloc(n)) ) ABORT("Malloc fails for perm_c[].");
@@ -57,10 +58,10 @@ int main(int argc, char *argv[])
     StatInit(&stat);
 
     /* Solve the linear system. */
-    dgssv(&options, &A, perm_c, perm_r, &L, &U, &B, &stat, &info);
-    dPrint_CompCol_Matrix("A", &A);
-    dPrint_CompCol_Matrix("U", &U);
-    dPrint_SuperNode_Matrix("L", &L);
+    sgssv(&options, &A, perm_c, perm_r, &L, &U, &B, &stat, &info);
+    sPrint_CompCol_Matrix("A", &A);
+    sPrint_CompCol_Matrix("U", &U);
+    sPrint_SuperNode_Matrix("L", &L);
     print_int_vec("\nperm_r", m, perm_r);
     
     /* De-allocate storage */

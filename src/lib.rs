5e6ed9c851d25bd65f1dd88b42f481bbbd8dec40
John Scott <johnrscott0@gmail.com>
2023-02-10 21:24:08 +0000

    Updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,11 @@
 //! of the underlying C library.
 //!
 //! The SuperLU User Guide is
-//! <https://portal.nersc.gov/project/sparse/superlu/superlu_ug.pdf>, and
-//! provides an overview of what SuperLU can do.
-//!
-//! SuperLU solves sparse systems of linear equations of the form $$AX = B,$$
-//! where $A$ is a sparse $n\times n$ matrix, $B$ is a dense
-//! $n \times n_\text{rhs}$ matrix of right-hand sides, and $X$ is the matrix
-//! of unknowns (the same size as $B$).
-//!
+//! [here](https://portal.nersc.gov/project/sparse/superlu/superlu_ug.pdf),
+//! and provides an overview of what SuperLU can do. SuperLU solves sparse
+//! systems of linear equations of the form $$AX = B,$$ where $A$ is a sparse
+//! $n\times n$ matrix, $B$ is a dense $n \times n_\text{rhs}$ matrix of
+//! right-hand sides, and $X$ is the matrix of unknowns (the same size as $B$).
 //!
 //! The (C) function reference for SuperLU is provided
 //! [here](https://portal.nersc.gov/project/sparse/superlu/superlu_code_html/index.html). 

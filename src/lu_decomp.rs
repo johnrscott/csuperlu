65302c78419871e504f295cf60725699cb4802f4
John Scott <johnrscott0@gmail.com>
2023-02-15 19:05:33 +0000

    Started adding a function to read the values of an LU decomposition

--- src/lu_decomp.rs
+++ src/lu_decomp.rs
@@ -1,4 +1,5 @@
 use crate::c::comp_col::CCreateCompColMatrix;
+use crate::c::super_matrix::{c_SCformat, c_NCformat};
 use crate::c::super_node::CSuperNodeMatrixUtils;
 use crate::comp_col::CompColMatrix;
 use crate::super_node::SuperNodeMatrix;
@@ -12,7 +13,13 @@ where P: CSuperNodeMatrixUtils<P> + CCreateCompColMatrix<P> {
 
 impl<P> LUDecomp<P>
 where P: CSuperNodeMatrixUtils<P> + CCreateCompColMatrix<P> {
-    pub fn from_matrices(l: SuperNodeMatrix<P>, u: CompColMatrix<P>) -> Self {
+    pub fn from_matrices(mut l: SuperNodeMatrix<P>, mut u: CompColMatrix<P>) -> Self {
+	let l_c_super_matrix = l.super_matrix();
+	let u_c_super_matrix = u.super_matrix();
+	assert!(l_c_super_matrix.nrow == u_c_super_matrix.nrow,
+		"Number of rows in L and U must match");
+	assert!(l_c_super_matrix.ncol == u_c_super_matrix.ncol,
+		"Number of columns in L and U must match");
 	Self {
 	    l,
 	    u,
@@ -21,5 +28,21 @@ where P: CSuperNodeMatrixUtils<P> + CCreateCompColMatrix<P> {
     pub fn print(&mut self) {
 	self.l.print("L");
 	self.u.print("U");
+    }
+    pub fn value(&mut self, row: usize, col: usize) {
+	let l_c_super_matrix = self.l.super_matrix();
+	let u_c_super_matrix = self.u.super_matrix();
+	assert!(row < l_c_super_matrix.nrow as usize,
+		"Row index out of range");
+	assert!(col < l_c_super_matrix.ncol as usize,
+		"Column index out of range");
+	let l_c_scformat = unsafe {
+	    &mut *(l_c_super_matrix.Store as *mut c_SCformat)
+	};
+	let u_c_ncformat = unsafe {
+	    &mut *(u_c_super_matrix.Store as *mut c_NCformat)
+	};
+	
+
     }
 }

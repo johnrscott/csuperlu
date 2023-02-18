ea34cbd907530d536814c88a021631bfa1be4740
John Scott <johnrscott0@gmail.com>
2023-02-18 16:55:38 +0000

    Added Harwell-Boeing structure

--- src/harwell_boeing.rs
+++ src/harwell_boeing.rs
@@ -12,8 +12,8 @@ struct HarwellBoeingHeader {
     key: String,
     /// Total number of data lines
     total_data_lines: i32,
-    /// Number of data lines for pointers
-    num_col_pointer_lines: i32,
+    /// Number of data lines for column offset
+    num_column_offset_lines: i32,
     /// Number of lines for row indices
     num_row_index_lines: i32,
     /// Number of lines for non-zero values
@@ -26,7 +26,7 @@ struct HarwellBoeingHeader {
     /// Number of rows in the matrix
     nrow: i32,
     /// Number of columns in the matrix
-    ncol: i32,
+    ncolumns: i32,
     /// Number of non-zero values in the matrix
     num_non_zeroes: i32,
     num_elemental_entries: i32,
@@ -38,6 +38,24 @@ struct HarwellBoeingHeader {
     /// Number of right-hand sides
     num_rhs: i32,
     num_row_indices: i32,
-    
-	
 }
+
+/// Sparse matrix stored in Harwell-Boeing format. Often, the
+/// matrix may not contain right-hand side information (in this
+/// case it stores a single sparse matrix in compressed-column
+/// format). Sometimes, the non-zero values may be omitted, in
+/// which case the matrix is called a "pattern matrix".
+/// 
+struct HarwellBoeingMatrix<P> {
+    /// The header describing the matrix format
+    header: HarwellBoeingHeader,
+    /// Offsets to the start of each column in the row_indices vector
+    column_offsets: Vec<i32>,
+    /// Row indices for non-zero values in each column
+    row_indices: Vec<i32>,
+    /// Non-zero values corresponding to entries in row_indices
+    non_zero_values: Option<Vec<P>>,
+    /// Right-hand side, starting guess, initial solutions
+    rhs_info: Option<Vec<P>>,
+}
+

f5e64918edb13c25cf2bf6d229996594dc6d576e
John Scott <johnrscott0@gmail.com>
2023-02-19 11:11:17 +0000

    Fixed the other examples

--- examples/float.rs
+++ examples/float.rs
@@ -14,7 +14,6 @@
 
 use csuperlu::c::options::{colperm_t, superlu_options_t};
 use csuperlu::c::stat::{c_StatPrint, SuperLUStat_t};
-use csuperlu::c::super_matrix::Mtype_t;
 use csuperlu::comp_col::CompColMatrix;
 use csuperlu::dense::DenseMatrix;
 use csuperlu::simple_driver::{simple_driver, SimpleSolution};
@@ -51,7 +50,7 @@ fn main() {
     // Make the RHS vector
     let nrhs = 1;
     let rhs = vec![1.0; m as usize];
-    let b = DenseMatrix::from_vectors(m, nrhs, rhs, Mtype_t::SLU_GE);
+    let b = DenseMatrix::from_vectors(m, nrhs, rhs);
 
     let mut options = superlu_options_t::new();
     options.ColPerm = colperm_t::NATURAL;

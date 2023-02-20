1093c64e1ce799f900fe4130c8d71aa706bb5bf3
John Scott <johnrscott0@gmail.com>
2023-02-20 22:12:10 +0000

    Build now succeeds

--- src/value_type.rs
+++ src/value_type.rs
@@ -180,6 +180,55 @@ impl ValueType<f64> for f64 {
             dPrint_CompCol_Matrix(what, a);
         }
     }
+    fn c_create_dense_matrix(
+        x: &mut MaybeUninit<c_SuperMatrix>,
+        m: i32,
+        n: i32,
+        values: &mut Vec<f64>,
+        ldx: i32,
+        mtype: Mtype_t,
+    ) {
+        unsafe {
+            dCreate_Dense_Matrix(
+                x.as_mut_ptr(),
+                m,
+                n,
+                values.as_mut_ptr(),
+                ldx,
+                Stype_t::SLU_DN,
+                Dtype_t::SLU_D,
+                mtype,
+            );
+        }
+    }
+
+    fn c_print_dense_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
+        unsafe {
+            dPrint_Dense_Matrix(what, a);
+        }
+    }
+    fn c_print_super_node_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
+        unsafe {
+            dPrint_SuperNode_Matrix(what, a);
+        }
+    }
+    fn c_simple_driver(
+	options: &mut superlu_options_t,
+	a: *mut c_SuperMatrix,
+	perm_c: &mut Vec<i32>,
+	perm_r: &mut Vec<i32>,
+	l: &mut c_SuperMatrix,
+	u: &mut c_SuperMatrix,
+	b: *mut c_SuperMatrix,
+	stat: &mut SuperLUStat_t,
+	info: &mut i32,
+    ) {
+	unsafe {
+            dgssv(options, a, perm_c.as_mut_ptr(), perm_r.as_mut_ptr(),
+		  l, u, b, stat, info);
+	}	
+    }
+
 }
 
 impl ValueType<num::Complex<f32>> for num::Complex<f32> {
@@ -213,6 +262,54 @@ impl ValueType<num::Complex<f32>> for num::Complex<f32> {
         unsafe {
             cPrint_CompCol_Matrix(what, a);
         }
+    }
+    fn c_create_dense_matrix(
+        x: &mut MaybeUninit<c_SuperMatrix>,
+        m: i32,
+        n: i32,
+        values: &mut Vec<num::Complex<f32>>,
+        ldx: i32,
+        mtype: Mtype_t,
+    ) {
+        unsafe {
+            cCreate_Dense_Matrix(
+                x.as_mut_ptr(),
+                m,
+                n,
+                values.as_mut_ptr() as *mut libc::c_float,
+                ldx,
+                Stype_t::SLU_DN,
+                Dtype_t::SLU_C,
+                mtype,
+            );
+        }
+    }
+
+    fn c_print_dense_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
+        unsafe {
+            cPrint_Dense_Matrix(what, a);
+        }
+    }
+    fn c_print_super_node_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
+        unsafe {
+            cPrint_SuperNode_Matrix(what, a);
+        }
+    }
+    fn c_simple_driver(
+	options: &mut superlu_options_t,
+	a: *mut c_SuperMatrix,
+	perm_c: &mut Vec<i32>,
+	perm_r: &mut Vec<i32>,
+	l: &mut c_SuperMatrix,
+	u: &mut c_SuperMatrix,
+	b: *mut c_SuperMatrix,
+	stat: &mut SuperLUStat_t,
+	info: &mut i32,
+    ) {
+	unsafe {
+            cgssv(options, a, perm_c.as_mut_ptr(), perm_r.as_mut_ptr(),
+		  l, u, b, stat, info);
+	}	
     }
 }
 
@@ -248,69 +345,7 @@ impl ValueType<num::Complex<f64>> for num::Complex<f64> {
             zPrint_CompCol_Matrix(what, a);
         }
     }
-}
 
-impl ValueType<f64> for f64 {
-    fn c_create_dense_matrix(
-        x: &mut MaybeUninit<c_SuperMatrix>,
-        m: i32,
-        n: i32,
-        values: &mut Vec<f64>,
-        ldx: i32,
-        mtype: Mtype_t,
-    ) {
-        unsafe {
-            dCreate_Dense_Matrix(
-                x.as_mut_ptr(),
-                m,
-                n,
-                values.as_mut_ptr(),
-                ldx,
-                Stype_t::SLU_DN,
-                Dtype_t::SLU_D,
-                mtype,
-            );
-        }
-    }
-
-    fn c_print_dense_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
-        unsafe {
-            dPrint_Dense_Matrix(what, a);
-        }
-    }
-}
-
-impl ValueType<num::Complex<f32>> for num::Complex<f32> {
-    fn c_create_dense_matrix(
-        x: &mut MaybeUninit<c_SuperMatrix>,
-        m: i32,
-        n: i32,
-        values: &mut Vec<num::Complex<f32>>,
-        ldx: i32,
-        mtype: Mtype_t,
-    ) {
-        unsafe {
-            cCreate_Dense_Matrix(
-                x.as_mut_ptr(),
-                m,
-                n,
-                values.as_mut_ptr() as *mut libc::c_float,
-                ldx,
-                Stype_t::SLU_DN,
-                Dtype_t::SLU_C,
-                mtype,
-            );
-        }
-    }
-
-    fn c_print_dense_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
-        unsafe {
-            cPrint_Dense_Matrix(what, a);
-        }
-    }
-}
-
-impl ValueType<num::Complex<f64>> for num::Complex<f64> {
     fn c_create_dense_matrix(
         x: &mut MaybeUninit<c_SuperMatrix>,
         m: i32,
@@ -338,74 +373,13 @@ impl ValueType<num::Complex<f64>> for num::Complex<f64> {
             zPrint_Dense_Matrix(what, a);
         }
     }
-}
 
-impl ValueType<f32> for f32 {
-}
-
-impl ValueType<f64> for f64 {
-    fn c_print_super_node_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
-        unsafe {
-            dPrint_SuperNode_Matrix(what, a);
-        }
-    }
-}
-
-impl ValueType<num::Complex<f32>> for num::Complex<f32> {
-    fn c_print_super_node_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
-        unsafe {
-            cPrint_SuperNode_Matrix(what, a);
-        }
-    }
-}
-
-impl ValueType<num::Complex<f64>> for num::Complex<f64> {
     fn c_print_super_node_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
         unsafe {
             zPrint_SuperNode_Matrix(what, a);
         }
     }
-}
 
-impl ValueType<f64> for f64 {
-    fn c_simple_driver(
-	options: &mut superlu_options_t,
-	a: *mut c_SuperMatrix,
-	perm_c: &mut Vec<i32>,
-	perm_r: &mut Vec<i32>,
-	l: &mut c_SuperMatrix,
-	u: &mut c_SuperMatrix,
-	b: *mut c_SuperMatrix,
-	stat: &mut SuperLUStat_t,
-	info: &mut i32,
-    ) {
-	unsafe {
-            dgssv(options, a, perm_c.as_mut_ptr(), perm_r.as_mut_ptr(),
-		  l, u, b, stat, info);
-	}	
-    }
-}
-
-impl ValueType<num::Complex<f32>> for num::Complex<f32> {
-    fn c_simple_driver(
-	options: &mut superlu_options_t,
-	a: *mut c_SuperMatrix,
-	perm_c: &mut Vec<i32>,
-	perm_r: &mut Vec<i32>,
-	l: &mut c_SuperMatrix,
-	u: &mut c_SuperMatrix,
-	b: *mut c_SuperMatrix,
-	stat: &mut SuperLUStat_t,
-	info: &mut i32,
-    ) {
-	unsafe {
-            cgssv(options, a, perm_c.as_mut_ptr(), perm_r.as_mut_ptr(),
-		  l, u, b, stat, info);
-	}	
-    }
-}
-
-impl ValueType<num::Complex<f64>> for num::Complex<f64> {
     fn c_simple_driver(
 	options: &mut superlu_options_t,
 	a: *mut c_SuperMatrix,

50f5889d2f5502948d5c530b50f259fae7f20343
John Scott <johnrscott0@gmail.com>
2023-02-22 16:55:57 +0000

    Refactoring functions in value_type

--- src/value_type.rs
+++ src/value_type.rs
@@ -9,30 +9,13 @@ use num::Num;
 
 use csuperlu_sys::{super_matrix::{c_SuperMatrix, Mtype_t, Stype_t, Dtype_t}, options::superlu_options_t, stat::SuperLUStat_t, comp_col::{sCreate_CompCol_Matrix, sPrint_CompCol_Matrix, dCreate_CompCol_Matrix, dPrint_CompCol_Matrix, cCreate_CompCol_Matrix, cPrint_CompCol_Matrix, zCreate_CompCol_Matrix, zPrint_CompCol_Matrix}, dense::{sCreate_Dense_Matrix, sPrint_Dense_Matrix, dCreate_Dense_Matrix, dPrint_Dense_Matrix, cCreate_Dense_Matrix, cPrint_Dense_Matrix, zCreate_Dense_Matrix, zPrint_Dense_Matrix}, super_node::{sPrint_SuperNode_Matrix, dPrint_SuperNode_Matrix, cPrint_SuperNode_Matrix, zPrint_SuperNode_Matrix}, simple_driver::{sgssv, dgssv, cgssv, zgssv}};
 
-/// Create compressed column matrix of particular precision
+/// Valid numerical value types for the C SuperLU library
 ///
-/// Trait for access to low level C functions from SuperLU, which
-/// dispatches correctly based on the desired precision (and picks
-/// the right value for the Dtype argument). This trait is necessary
-/// because the function names for different precisions are
-/// different.
-///
-/// The assumption of this trait is that it is necessary to pass the
-/// correct value of Dtype corresponding to the prefix in front of
-/// the function (d, s, c, z). This would make the Dtype argument redundant,
-/// but I don't understand what the purpose of it is otherwise; the
-/// SuperLU functions do not allocate their own memory for vectors,
-/// so they cannot perform a precision conversion
-/// (one hypothesis for the Dtype argument), and it seems to lead to
-/// seg faults if the "wrong" Dtype is passed.
-///
-/// For a similar reason, the Stype parameter is also omitted, because this
-/// is always SLU_NC for this function (as stated in the doxygen docs).
-///
-/// Rename this to something like CompColUtils
 pub trait ValueType<P>: Num + Copy + FromStr + std::fmt::Debug {
+
+    /// Create a compressed-column matrix from raw vectors
+    ///
     fn c_create_comp_col_matrix(
-        a: &mut MaybeUninit<c_SuperMatrix>,
         m: i32,
         n: i32,
         nnz: i32,
@@ -40,8 +23,13 @@ pub trait ValueType<P>: Num + Copy + FromStr + std::fmt::Debug {
         rowind: &mut Vec<i32>,
         colptr: &mut Vec<i32>,
         mtype: Mtype_t,
-    );
-    fn c_print_comp_col_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix);
+    ) -> c_SuperMatrix;
+
+    /// Print a compressed-column matrix (from SuperLU library)
+    ///
+    /// The function makes the assumption that the C library does
+    /// not modify the arguments.
+    fn c_print_comp_col_matrix(what: &libc::c_char, a: &c_SuperMatrix);
     fn c_create_dense_matrix(
         x: &mut MaybeUninit<c_SuperMatrix>,
         m: i32,
@@ -50,8 +38,8 @@ pub trait ValueType<P>: Num + Copy + FromStr + std::fmt::Debug {
         ldx: i32,
         mtype: Mtype_t,
     );
-    fn c_print_dense_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix);
-    fn c_print_super_node_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix);
+    fn c_print_dense_matrix(what: &libc::c_char, a: &c_SuperMatrix);
+    fn c_print_super_node_matrix(what: &libc::c_char, a: &c_SuperMatrix);
     fn c_simple_driver(
 	options: &mut superlu_options_t,
 	a: *mut c_SuperMatrix,
@@ -67,7 +55,6 @@ pub trait ValueType<P>: Num + Copy + FromStr + std::fmt::Debug {
 
 impl ValueType<f32> for f32 {
     fn c_create_comp_col_matrix(
-        a: &mut MaybeUninit<c_SuperMatrix>,
         m: i32,
         n: i32,
         nnz: i32,
@@ -75,10 +62,11 @@ impl ValueType<f32> for f32 {
         rowind: &mut Vec<i32>,
         colptr: &mut Vec<i32>,
         mtype: Mtype_t,
-    ) {
+    ) -> c_SuperMatrix {
+	let mut a = c_SuperMatrix::alloc();
         unsafe {
             sCreate_CompCol_Matrix(
-                a.as_mut_ptr(),
+                a as *mut c_SuperMatrix,
                 m,
                 n,
                 nnz,
@@ -90,11 +78,13 @@ impl ValueType<f32> for f32 {
                 mtype,
             );
         }
+	a
     }
 
-    fn c_print_comp_col_matrix(what: *mut libc::c_char, a: *mut c_SuperMatrix) {
+    fn c_print_comp_col_matrix(what: &libc::c_char, a: &c_SuperMatrix) {
         unsafe {
-            sPrint_CompCol_Matrix(what, a);
+            sPrint_CompCol_Matrix(what as *const libc::c_char as *mut libc::c_char,
+				  a as *const c_SuperMatrix as *mut c_SuperMatrix);
         }
     }
     fn c_create_dense_matrix(

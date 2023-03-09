964e2dff0edd27b95462802a785e85d8e6367ce5
lanamineh <33867817+lanamineh@users.noreply.github.com>
2023-03-09 18:06:08 +0000

    Some name refactoring

--- src/sparse_matrix.rs
+++ src/sparse_matrix.rs
@@ -7,72 +7,72 @@ use std::collections::HashMap;
 use crate::c::value_type::ValueType;
 
 #[derive(Debug)]
-pub struct SparseMatrix<P: ValueType> {
-    nrows: usize,
-    ncols: usize,
-    values: HashMap<(usize, usize), P>,
+pub struct SparseMat<P: ValueType> {
+    num_rows: usize,
+    num_cols: usize,
+    non_zero_vals: HashMap<(usize, usize), P>,
 }
 
-impl<P: ValueType> SparseMatrix<P> {
+impl<P: ValueType> SparseMat<P> {
     /// Create an empty sparse matrix with zero rows and columns.
     pub fn empty() -> Self {
         Self {
-            nrows: 0,
-            ncols: 0,
-            values: HashMap::new(),
+            num_rows: 0,
+            num_cols: 0,
+            non_zero_vals: HashMap::new(),
         }
     }
 
     /// Create an empty sparse matrix of the given size.
-    pub fn new(nrows: usize, ncols: usize) -> Self {
+    pub fn new(num_rows: usize, num_cols: usize) -> Self {
         Self {
-            nrows,
-            ncols,
-            values: HashMap::new(),
+            num_rows,
+            num_cols,
+            non_zero_vals: HashMap::new(),
         }	
     }
 
     /// Input a triplet into the sparse matrix, checking the row and column against the matrix size.
     pub fn insert(&mut self, row: usize, col: usize, val: P) {
-	if row >= self.nrows || col >= self.ncols {
+	if row >= self.num_rows || col >= self.num_cols {
 	    panic!("Triplet index ({}, {}) out of range for matrix size {}x{}",
-		   row, col, self.nrows, self.ncols);
+		   row, col, self.num_rows, self.num_cols);
 	}
 
 	if val == P::zero() {
-	    if self.values.contains_key(&(row, col)) {
-		self.values.remove(&(row, col));
+	    if self.non_zero_vals.contains_key(&(row, col)) {
+		self.non_zero_vals.remove(&(row, col));
 	    }
 	}
 	else {
-            self.values.insert((row, col), val);
+            self.non_zero_vals.insert((row, col), val);
 	}
     }
 
     /// Read the value at the given row and column.
     pub fn get(&self, row: usize, col: usize) -> P {
-	if row >= self.nrows || col >= self.ncols {
+	if row >= self.num_rows || col >= self.num_cols {
 	    panic!("Triplet index ({}, {}) out of range for matrix size {}x{}",
-		   row, col, self.nrows, self.ncols);
+		   row, col, self.num_rows, self.num_cols);
         }
-        self.values.get(&(row, col)).copied().unwrap_or(P::zero())
+        self.non_zero_vals.get(&(row, col)).copied().unwrap_or(P::zero())
     }
     
     /// Input a triplet into the sparse matrix, allowing the matrix to automatically
     /// resize to fit the new element.
     pub fn insert_unbounded(&mut self, row: usize, col: usize, val: P) {
 	if val == P::zero() {
-	    if self.values.contains_key(&(row, col)) {
-		self.values.remove(&(row, col));
+	    if self.non_zero_vals.contains_key(&(row, col)) {
+		self.non_zero_vals.remove(&(row, col));
 	    }
 	}
 	else {
-            self.values.insert((row, col), val);
-	    if row >= self.nrows {
-		self.nrows = row + 1;
+            self.non_zero_vals.insert((row, col), val);
+	    if row >= self.num_rows {
+		self.num_rows = row + 1;
 	    }
-	    if col >= self.ncols {
-		self.ncols = col + 1;
+	    if col >= self.num_cols {
+		self.num_cols = col + 1;
 	    }
 	}
     }
@@ -81,45 +81,50 @@ impl<P: ValueType> SparseMatrix<P> {
     /// the row and column are within the bounds of the matrix -- it will just return 0
     /// even if it is out of bounds.
     pub fn get_unbounded(&self, row: usize, col: usize) -> P {
-	self.values.get(&(row, col)).copied().unwrap_or(P::zero())
+	self.non_zero_vals.get(&(row, col)).copied().unwrap_or(P::zero())
     }
 
     /// Get the number of rows in the matrix.
-    pub fn nrows(&self) -> usize {
-	self.nrows
+    pub fn num_rows(&self) -> usize {
+	self.num_rows
     }
 
     /// Get the number of columns in the matrix.
-    pub fn ncols(&self) -> usize {
-	self.ncols
+    pub fn num_cols(&self) -> usize {
+	self.num_cols
     }
 
     /// Get the number of non-zero values in the matrix.
-    pub fn nnz(&self) -> usize {
-	self.values.len()
+    pub fn num_non_zeros(&self) -> usize {
+	self.non_zero_vals.len()
+    }
+
+    pub fn non_zero_vals(&self) -> &HashMap<(usize, usize), P> {
+	&self.non_zero_vals
     }
 }
 
-impl<P: ValueType> fmt::Display for SparseMatrix<P> {
+impl<P: ValueType> fmt::Display for SparseMat<P> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-	writeln!(f, "{} x {} matrix, {} non-zero values", self.nrows, self.ncols, self.values.len())?;
-	let sorted_keys = self.values.keys()
+	writeln!(f, "{} x {} matrix, {} non-zero values",
+		 self.num_rows, self.num_cols, self.num_non_zeros())?;
+	let sorted_keys = self.non_zero_vals.keys()
 	    .sorted_unstable_by_key(|a| (a.1, a.0)); 
 	for key in sorted_keys {
-	    writeln!(f, "({}, {}) = {:?}", key.0, key.1, self.values[key])?;
+	    writeln!(f, "({}, {}) = {:?}", key.0, key.1, self.non_zero_vals[key])?;
 	}
 	Ok(())
     }
 }
 
-impl<P: ValueType> From<HashMap<(usize, usize), P>> for SparseMatrix<P> {
-    fn from(values: HashMap<(usize, usize), P>) -> Self {
-	let nrows = values.keys().max_by_key(|k| k.0).unwrap().0 + 1;
-	let ncols = values.keys().max_by_key(|k| k.1).unwrap().1 + 1;
+impl<P: ValueType> From<HashMap<(usize, usize), P>> for SparseMat<P> {
+    fn from(non_zero_vals: HashMap<(usize, usize), P>) -> Self {
+	let num_rows = non_zero_vals.keys().max_by_key(|k| k.0).unwrap().0 + 1;
+	let num_cols = non_zero_vals.keys().max_by_key(|k| k.1).unwrap().1 + 1;
 	Self {
-	    nrows,
-	    ncols,
-	    values,
+	    num_rows,
+	    num_cols,
+	    non_zero_vals,
 	}
     }
 }
--- src/sparse_matrix/tests.rs
+++ src/sparse_matrix/tests.rs
@@ -3,35 +3,35 @@ use super::*;
 
 #[test]
 fn empty_matrix() {
-    let a = SparseMatrix::<f64>::empty();
-    assert_eq!(a.nrows, 0);
-    assert_eq!(a.nrows(), 0);
-    assert_eq!(a.ncols, 0);
-    assert_eq!(a.ncols(), 0);
-    assert_eq!(a.values.len(), 0);
-    assert_eq!(a.nnz(), 0);
+    let a = SparseMat::<f64>::empty();
+    assert_eq!(a.num_rows, 0);
+    assert_eq!(a.num_rows(), 0);
+    assert_eq!(a.num_cols, 0);
+    assert_eq!(a.num_cols(), 0);
+    assert_eq!(a.non_zero_vals.len(), 0);
+    assert_eq!(a.num_non_zeros(), 0);
 }
 
 #[test]
 fn new_matrix() {
-    let a = SparseMatrix::<f64>::new(5, 8);
-    assert_eq!(a.nrows, 5);
-    assert_eq!(a.nrows(), 5);
-    assert_eq!(a.ncols, 8);
-    assert_eq!(a.ncols(), 8);
-    assert_eq!(a.values.len(), 0);
-    assert_eq!(a.nnz(), 0);
+    let a = SparseMat::<f64>::new(5, 8);
+    assert_eq!(a.num_rows, 5);
+    assert_eq!(a.num_rows(), 5);
+    assert_eq!(a.num_cols, 8);
+    assert_eq!(a.num_cols(), 8);
+    assert_eq!(a.non_zero_vals.len(), 0);
+    assert_eq!(a.num_non_zeros(), 0);
 }
 
 #[test]
 fn insert_into_fixed_matrix() {
-    let mut a = SparseMatrix::<f64>::new(5, 5);
+    let mut a = SparseMat::<f64>::new(5, 5);
     a.insert(1, 3, 1.5);
     a.insert(4, 2, 0.4);
-    assert_eq!(a.values.len(), 2);
-    assert_eq!(a.nnz(), 2);
+    assert_eq!(a.non_zero_vals.len(), 2);
+    assert_eq!(a.num_non_zeros(), 2);
     assert_eq!(a.get(1, 3), 1.5);
-    assert_eq!(a.values[&(1, 3)], 1.5);
+    assert_eq!(a.non_zero_vals[&(1, 3)], 1.5);
     assert_eq!(a.get(4, 2), 0.4);
     for i in 0..5 {
 	for j in 0..5 {
@@ -45,28 +45,28 @@ fn insert_into_fixed_matrix() {
 #[test]
 #[should_panic]
 fn invalid_insert_into_fixed_matrix() {
-    let mut a = SparseMatrix::<f64>::new(5, 5);
+    let mut a = SparseMat::<f64>::new(5, 5);
     a.insert(6, 1, 1.9);
 }
 
 #[test]
 #[should_panic]
 fn invalid_read_of_fixed_matrix() {
-    let mut a = SparseMatrix::<f64>::new(5, 5);
+    let mut a = SparseMat::<f64>::new(5, 5);
     a.insert(4, 1, 1.9);
     let _ = a.get(6, 7);
 }
 
 #[test]
 fn insert_into_dynamic_matrix() {
-    let mut a = SparseMatrix::<f64>::empty();
+    let mut a = SparseMat::<f64>::empty();
     a.insert_unbounded(1, 3, 1.5);
     a.insert_unbounded(4, 2, 0.4);
-    assert_eq!(a.nrows(), 5);
-    assert_eq!(a.ncols(), 4);
-    assert_eq!(a.nnz(), 2);
+    assert_eq!(a.num_rows(), 5);
+    assert_eq!(a.num_cols(), 4);
+    assert_eq!(a.num_non_zeros(), 2);
     assert_eq!(a.get(1, 3), 1.5);
-    assert_eq!(a.values[&(1, 3)], 1.5);
+    assert_eq!(a.non_zero_vals[&(1, 3)], 1.5);
     assert_eq!(a.get(4, 2), 0.4);
     // Check up to 5x5 even though it isn't that size
     for i in 0..5 {
@@ -84,10 +84,10 @@ fn matrix_from_hashmap() {
     a.insert((2, 2), 0.5);
     a.insert((3, 1), 1.5);
     a.insert((0, 1), 1.3);
-    let b = SparseMatrix::from(a);
-    assert_eq!(b.nrows(), 4);
-    assert_eq!(b.ncols(), 3);
-    assert_eq!(b.nnz(), 3);
+    let b = SparseMat::from(a);
+    assert_eq!(b.num_rows(), 4);
+    assert_eq!(b.num_cols(), 3);
+    assert_eq!(b.num_non_zeros(), 3);
     assert_eq!(b.get(2, 2), 0.5);
     assert_eq!(b.get(3, 1), 1.5);
     assert_eq!(b.get(0, 1), 1.3);

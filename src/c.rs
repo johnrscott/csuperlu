5f40455f2b8c08f3a17a97d1b78d900ba748bf2a
John Scott <johnrscott0@gmail.com>
2023-03-09 18:13:22 +0000

    Pulled the latest sparse_matrix from sparse_matrix branch

---
+++ src/c.rs
@@ -0,0 +1,14 @@
+//! C-to-Rust interface for SuperLU
+//!
+//! This module contains a low-level interface to the csuperlu_sys
+//! library. The purpose is to expose the functions in csuperlu_sys
+//! in a form with native rust types, using a rust naming convention
+//! for names. Functions which depend on precision (many of them) are
+//! defined inside the ValueType trait. Most functions are unsafe.
+//!
+
+pub mod value_type;
+pub mod free;
+pub mod options;
+pub mod stat;
+pub mod super_matrix;
--- src/sparse_matrix.rs
+++ src/sparse_matrix.rs
@@ -1,154 +1,133 @@
+//! Create and manipulate sparse matrices
+
 use itertools::Itertools;
 use std::fmt;
 use std::collections::HashMap;
 
-use crate::{comp_col::CompColMatrix, c::value_type::ValueType};
+use crate::c::value_type::ValueType;
 
 #[derive(Debug)]
-pub struct SparseMatrix<P: ValueType> {
+pub struct SparseMat<P: ValueType> {
     num_rows: usize,
     num_cols: usize,
-    values: HashMap<(usize, usize), P>,
+    non_zero_vals: HashMap<(usize, usize), P>,
 }
 
-impl<P: ValueType> SparseMatrix<P> {
-    pub fn new() -> Self {
+impl<P: ValueType> SparseMat<P> {
+    /// Create an empty sparse matrix with zero rows and columns.
+    pub fn empty() -> Self {
         Self {
             num_rows: 0,
             num_cols: 0,
-            values: HashMap::new(),
+            non_zero_vals: HashMap::new(),
         }
     }
 
-    pub fn from_dict_of_keys(values: HashMap<(usize, usize), P>) -> Self {
-	let num_rows = values.iter().max_by_key(|entry| entry.0.0).unwrap().0.0 + 1;
-	let num_cols = values.iter().max_by_key(|entry| entry.0.1).unwrap().0.1 + 1;
-	Self {
-	    num_rows,
-	    num_cols,
-	    values
-	}
+    /// Create an empty sparse matrix of the given size.
+    pub fn new(num_rows: usize, num_cols: usize) -> Self {
+        Self {
+            num_rows,
+            num_cols,
+            non_zero_vals: HashMap::new(),
+        }	
     }
 
-    // TODO: Can we overload something to make the input nicer, e.g a[row, col] = value
-    pub fn set_value(&mut self, row: usize, col: usize, value: P) {
-	// Keep track of new size
-	if row >= self.num_rows {
-	    self.num_rows = row + 1;
+    /// Input a triplet into the sparse matrix, checking the row and column against the matrix size.
+    pub fn insert(&mut self, row: usize, col: usize, val: P) {
+	if row >= self.num_rows || col >= self.num_cols {
+	    panic!("Triplet index ({}, {}) out of range for matrix size {}x{}",
+		   row, col, self.num_rows, self.num_cols);
 	}
-	if col >= self.num_cols {
-	    self.num_cols = col + 1;
-	}	
-	
-	if value == P::zero() {
-	    if self.values.contains_key(&(row, col)) {
-		self.values.remove(&(row, col));
+
+	if val == P::zero() {
+	    if self.non_zero_vals.contains_key(&(row, col)) {
+		self.non_zero_vals.remove(&(row, col));
 	    }
 	}
 	else {
-            self.values.insert((row, col), value);
+            self.non_zero_vals.insert((row, col), val);
 	}
     }
 
-    pub fn get_value(&self, row: usize, col: usize) -> P {
-        if row >= self.num_rows || col >= self.num_cols {
-            panic!("Index out of range");
+    /// Read the value at the given row and column.
+    pub fn get(&self, row: usize, col: usize) -> P {
+	if row >= self.num_rows || col >= self.num_cols {
+	    panic!("Triplet index ({}, {}) out of range for matrix size {}x{}",
+		   row, col, self.num_rows, self.num_cols);
         }
-        self.values.get(&(row, col)).copied().unwrap_or(P::zero())
+        self.non_zero_vals.get(&(row, col)).copied().unwrap_or(P::zero())
     }
-
-    pub fn compressed_column_format(&self) -> CompColMatrix<P> {	
-	// Sort in column order
-	let sorted_keys = self.values.keys()
-	    .sorted_unstable_by_key(|a| (a.1, a.0)); 
-
-	let num_non_zeros = self.values.len();
-	let mut non_zero_values = Vec::<P>::with_capacity(num_non_zeros);
-	let mut column_offsets = Vec::<i32>::with_capacity(self.num_cols + 1);
-	let mut row_indices = Vec::<i32>::with_capacity(num_non_zeros);
-
-	column_offsets.push(0);
-	let mut current_col = 0usize;
-	
-	for key in sorted_keys {
-	    if key.1 > current_col {
-		// Handle empty columns with this range
-		for _ in 0..(key.1 - current_col) {
-		    column_offsets.push(non_zero_values.len() as i32);
-		}
-		current_col = key.1;
+    
+    /// Input a triplet into the sparse matrix, allowing the matrix to automatically
+    /// resize to fit the new element.
+    pub fn insert_unbounded(&mut self, row: usize, col: usize, val: P) {
+	if val == P::zero() {
+	    if self.non_zero_vals.contains_key(&(row, col)) {
+		self.non_zero_vals.remove(&(row, col));
 	    }
-	    non_zero_values.push(self.values[key]);
-	    row_indices.push(key.0 as i32);
 	}
-	column_offsets.push(num_non_zeros as i32);
-
-	CompColMatrix::from_vectors(self.num_rows, non_zero_values, row_indices, column_offsets)
+	else {
+            self.non_zero_vals.insert((row, col), val);
+	    if row >= self.num_rows {
+		self.num_rows = row + 1;
+	    }
+	    if col >= self.num_cols {
+		self.num_cols = col + 1;
+	    }
+	}
     }
 
+    /// Read the value at the given row and column. This function won't check if
+    /// the row and column are within the bounds of the matrix -- it will just return 0
+    /// even if it is out of bounds.
+    pub fn get_unbounded(&self, row: usize, col: usize) -> P {
+	self.non_zero_vals.get(&(row, col)).copied().unwrap_or(P::zero())
+    }
+
+    /// Get the number of rows in the matrix.
     pub fn num_rows(&self) -> usize {
 	self.num_rows
     }
 
+    /// Get the number of columns in the matrix.
     pub fn num_cols(&self) -> usize {
 	self.num_cols
     }
 
+    /// Get the number of non-zero values in the matrix.
     pub fn num_non_zeros(&self) -> usize {
-	self.values.len()
+	self.non_zero_vals.len()
     }
 
-    pub fn values(&self) -> &HashMap<(usize, usize), P> {
-	&self.values
+    pub fn non_zero_vals(&self) -> &HashMap<(usize, usize), P> {
+	&self.non_zero_vals
     }
-
-    pub fn print_structure(&self) {
-	for r in 0..self.num_rows {
-	    for c in 0..self.num_cols {
-		let res = self.values.get(&(r, c));
-		match res {
-		    None => print!("  "),
-		    Some(_) => print!("x "),
-		}
-	    }
-	    print!("\n");
-	}
-    }
-
-    /// Allow resizing (shrinking and expanding) as long as contents are preserved
-    /// Pads with additional rows and columns to fit new size
-    pub fn resize(&mut self, num_rows: usize, num_cols: usize) {
-	self.resize_rows(num_rows);
-	self.resize_cols(num_cols);
-    }
-
-    pub fn resize_rows(&mut self, num_rows: usize) {
-	let num_rows_actual = self.values.iter().max_by_key(|entry| entry.0.0).unwrap().0.0 + 1;
-	if num_rows < num_rows_actual {
-	    panic!("Cannot resize matrix (rows) to be smaller than its contents");
-	}
-	self.num_rows = num_rows;
-    }
-
-    pub fn resize_cols(&mut self, num_cols: usize) {
-	let num_cols_actual = self.values.iter().max_by_key(|entry| entry.0.1).unwrap().0.1 + 1;
-	if num_cols < num_cols_actual {
-	    panic!("Cannot resize matrix (cols) to be smaller than its contents");
-	}
-	self.num_cols = num_cols;
-    }
-
 }
 
-impl<P: ValueType> fmt::Display for SparseMatrix<P> {
+impl<P: ValueType> fmt::Display for SparseMat<P> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-	writeln!(f, "{} x {} matrix, {} non-zero values", self.num_rows, self.num_cols, self.values.len())?;
-	let sorted_keys = self.values.keys()
+	writeln!(f, "{} x {} matrix, {} non-zero values",
+		 self.num_rows, self.num_cols, self.num_non_zeros())?;
+	let sorted_keys = self.non_zero_vals.keys()
 	    .sorted_unstable_by_key(|a| (a.1, a.0)); 
 	for key in sorted_keys {
-	    writeln!(f, "({}, {}) = {:?}", key.0, key.1, self.values[key])?;
+	    writeln!(f, "({}, {}) = {:?}", key.0, key.1, self.non_zero_vals[key])?;
 	}
 	Ok(())
     }
 }
 
+impl<P: ValueType> From<HashMap<(usize, usize), P>> for SparseMat<P> {
+    fn from(non_zero_vals: HashMap<(usize, usize), P>) -> Self {
+	let num_rows = non_zero_vals.keys().max_by_key(|k| k.0).unwrap().0 + 1;
+	let num_cols = non_zero_vals.keys().max_by_key(|k| k.1).unwrap().1 + 1;
+	Self {
+	    num_rows,
+	    num_cols,
+	    non_zero_vals,
+	}
+    }
+}
+
+#[cfg(test)]
+mod tests;
